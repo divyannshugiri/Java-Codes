f73a8c47ccc6de184371e36f45bfec1138a84e40
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-11-28 09:34:20 +0000

    Commit Successful

--- Vowel.java
+++ Vowel.java
@@ -1,28 +1,35 @@
-Java Program Vowel Or Consonant
+//Java Program Vowel Or Consonant
 
 import java.util.Scanner;
 
 
 public class Vowel {
 
-    public ststic void main (String args[])
+    public static void main (String args[])
 
     {
         Scanner sc=new Sanner(System.in);
 
         System.out.println("Enter a character to check it is vowel or consonant: ");
 
-      char ch =sc.next();
+      char ch =sc.next().charAt(0);
 
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+        || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch=='U');
+
+        else if(ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) 
         {
-            System.out.println("It is a Vowel");
+            System.out.println("It is a Consonant");
+        } 
+        else 
+        {
+            System.out.println("Invalid input. Please enter an alphabet character.");
         }
-        else
-         {
-            System.out.println("It is a consonant");
-         }
 
+        sc.close(); 
     }
-    
 }
+
+
+
+   
