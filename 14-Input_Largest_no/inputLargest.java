cc97b04d38748af4b5ea0c5b30ac2277a2276290
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-11-29 18:31:39 +0000

    Commit Successful

---
+++ 14-Input_Largest_no/inputLargest.java
@@ -0,0 +1,37 @@
+//Take integer inputs till the user enters 0 and print the largest number from all.
+
+import java.util.Scanner;
+
+public class inputLargest {
+                                                  //  loop while, input, num>max,max=num,enters 0 ,terminate,print max
+    public static void main(String[] args) {                                     
+        
+        Scanner sc =new Scanner(System.in);
+        
+        int max=0;
+
+        while(true)
+        {
+            int i =sc.nextInt();
+          
+            
+            if(i>max)
+            {
+                max=i;
+            }
+           
+            if(i==0)
+            {
+            System.out.println("User enters 0");
+            
+            break;
+            }
+        }
+           
+           System.out.println("Largest No from the input given is: "+max);      
+           sc.close();
+        }
+
+        }
+       
+        
