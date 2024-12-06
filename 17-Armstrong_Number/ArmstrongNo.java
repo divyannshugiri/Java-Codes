78848ddf4d71d0332538c2b7b3516a2ab6ec9719
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-06 15:06:26 +0000

    Commit Successful

--- 17-Armstrong_Number/ArmstrongNo.java
+++ 17-Armstrong_Number/ArmstrongNo.java
@@ -1,9 +1,47 @@
 import java.util.Scanner;
 
-public class ArmstrongNo {
+public class ArmstrongNo 
+{
+ public static void main(String[] args) 
+ {
+ Scanner sc =new Scanner(System.in);
+ int z,count=0,digit,i;
+ System.out.println("Enter a Number");
+ int n =sc.nextInt();
+ z=n;
+while(z>0)                                         // to check digits in a number 
+ {
+   count=count+1;                                  
+   z=z/10;
+  
+ }  
 
-    public static void main(String[] args)
-     {
-        Scanner sc = new Scanner(System.in);
-     }
+ z=n;                                          // to get reminder and getting the power of it and adding to sum
+ 
+ int sum=0;                                      
+ 
+ while(z>0)                                   //153>0   true
+   {
+     digit=z%10;                              // digit=153%10         will get reminder  3      5 
+      int pro=1;                          
+        
+      for(i=1;i<=count;i++)                   // 1<=3,  true          1<=5   true
+         {
+         pro=pro*digit;                       // 1=1*3    result 3  9   27        1=1*5  result 5  25       125
+         }                    
+        
+         sum=sum+pro;                          // sum=27                      sum=125+25          sum=150
+         z=z/10;                               // 153/10= 15                  15/10=1
+   }
+
+
+   if(sum==n)
+   {
+      System.out.println("It is a Armstrong Number");
+   }
+   else
+   {
+      System.out.println("It is not a Armstrong Number");
+   }
+}
 }
