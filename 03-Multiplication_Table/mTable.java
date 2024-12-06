4a0f0453192e777e5cfc719f144ad9ad143ea86f
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-06 18:05:47 +0000

    Commit Successful

--- 03-Multiplication_Table/mTable.java
+++ 03-Multiplication_Table/mTable.java
@@ -10,7 +10,7 @@ public static void main(String[] args)
     int x=sc.nextInt();
      
     for(int y=2;y<=10;y++)             //To multiply from 2 because input gets printed 1 times 
-                 {                     //and multiplication also start from 1 therfore it printed 2 times to avoid this we multiply from 2 
+                 {                    
         System.out.println(x*y);
     }
     sc.close();
