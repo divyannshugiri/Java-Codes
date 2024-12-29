196abe390ed750e74aba795887df12019e5da207
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-29 17:16:23 +0000

    Commit Successful

---
+++ 20-Palindrome_Strings/strpal.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class strpal
+{
+
+public static void main(String [] args) 
+{
+    Scanner sc =new Scanner(System.in);
+
+    System.out.println("Enter a String to check for palindrome");
+     
+    String name = sc.nextLine();
+
+    int leng = name.length();
+
+    String rev ="";
+
+    for(int i=leng-1;i>=0;i--)
+    {
+        rev=rev+name.charAt(i);
+    }
+    
+    if (name.equals(rev))
+    {
+        System.out.println("It is a Palindrome String");
+
+    }
+    else
+    {
+        System.out.println("Not a Palindrome String");
+    }
+
+    sc.close();
+}
+
+
+}
+
+
