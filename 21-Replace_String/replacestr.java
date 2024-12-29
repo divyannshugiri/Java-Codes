673d7c06abdd9c8db68147cebd6ede6db5e2fffe
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-29 18:18:11 +0000

    Commit Successful

---
+++ 21-Replace_String/replacestr.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+
+public class replacestr
+{
+public static void main(String[] args) 
+{
+   // Scanner sc =new Scanner((System.in);
+
+    String str ="Hello World";
+
+    String replaced = str.replace("World","Divyanshu");
+    
+    System.out.println("String Given is : "+str);
+    System.out.println("Replace String is : "+replaced);
+    
+}
+
+}
