57bf55e94bb26940f5d7312ba789dcc037deb178
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-29 17:48:09 +0000

    Commit Successful

--- 09-Vowel or Consonant/Vowel.java
+++ 09-Vowel or Consonant/Vowel.java
@@ -32,6 +32,31 @@ public class Vowel {
     }
 }
 
-/* Alternative Program */
+/* Alternative Program 
+ 
+import java.util.Scanner;
+
+public class Vowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character to check if it is a vowel or consonant: ");
+        
+        char ch = sc.next().toLowerCase().charAt(0);  // Convert input to lowercase for simplicity
+        
+        if ("aeiou".indexOf(ch) != -1) {
+            System.out.println("It is Vowel");
+        } else if (Character.isLetter(ch)) {
+            System.out.println("It is a Consonant");
+        } else {
+            System.out.println("Invalid input. Please enter an alphabet character.");
+        }
+        
+        sc.close();
+    }
+}
+
+
+
+*/
 
    
