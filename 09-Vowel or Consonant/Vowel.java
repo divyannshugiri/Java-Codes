2d9476a324ea9f92d1f8eec836f47b81113bc0c3
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-29 17:38:21 +0000

    Commit Successful

--- 09-Vowel or Consonant/Vowel.java
+++ 09-Vowel or Consonant/Vowel.java
@@ -15,8 +15,10 @@ public class Vowel {
       char ch =sc.next().charAt(0);
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
-        || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch=='U');
-
+        || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch=='U')
+        {
+            System.out.println("It is Vowel");
+        }
         else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) 
         {
             System.out.println("It is a Consonant");
@@ -30,6 +32,6 @@ public class Vowel {
     }
 }
 
-
+/* Alternative Program */
 
    
