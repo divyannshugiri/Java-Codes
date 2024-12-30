24a12a6540d26db4b02936830536ae050e04a2af
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-30 19:28:49 +0000

    Commit Successful

---
+++ Basic_Arrays/basicarrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class basicarrays
+{
+    public static void main(String[] args) {
+        
+        int a[]={21,22,23,24,25};
+
+        for(int i=0;i<a.length;i++){
+
+        System.out.println(a[i]);
+        }
+    }
+}
