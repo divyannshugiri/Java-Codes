5b574f500a5320800f8d815fd33db4f0e1152b39
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-30 16:20:07 +0000

    Commit Successful

--- 21-Replace_String/replacestr.java
+++ 21-Replace_String/replacestr.java
@@ -1,10 +1,45 @@
-//import java.util.Scanner;
+import java.util.Scanner;
+
+public class replacestr
+{
+public static void main(String[] args)
+ {
+    Scanner sc =new Scanner(System.in);
+
+    System.out.println("Enter a String");
+
+    String str=sc.nextLine();
+
+    System.out.println("Enter a word to replace");
+    
+    String oldWord=sc.nextLine();
+
+    System.out.println("Enter a new word to replace with");
+
+    String newWord=sc.nextLine();
+
+    String replace = str.replace(oldWord,newWord);
+
+    System.out.println("String Given is    :"+str);
+    System.out.println("Replaced String is : "+replace);
+
+    sc.close();
+
+}
+
+}
+
+
+
+
+
+/*import java.util;
 
 public class replacestr
 {
 public static void main(String[] args) 
 {
-   // Scanner sc =new Scanner((System.in);
+   
 
     String str ="Hello World";
 
@@ -15,4 +50,4 @@ public static void main(String[] args)
     
 }
 
-}
+}*/
