bade8263fd1f81af7486436140689aeae8abf919
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2024-12-31 18:30:20 +0000

    Commit Successful

---
+++ 25-LinearSearch_Array/linearsearcharr.java
@@ -0,0 +1,13 @@
+
+
+public class linearsearcharr
+{
+    public static linearsearch(int numbers[],int key)
+    {
+        for(int i=0;i<numbers.length;i++)
+    }
+    public static void main(String[] args) {
+        int numbers[] = {20,29,35,44,47,50,31,22,39,19};
+        int key = 22;
+    }
+}
