9bdf2ad152f1e57c3653b613be37a4b9387c782c
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-01 10:18:21 +0000

    Commit Successful

--- 26-LargestNumber_Array/largestnumber.java
+++ 26-LargestNumber_Array/largestnumber.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class largestnumber{
     public static int getlargest(int number[]){
         int largest = Integer.MIN_VALUE;
+        int  smallest = Integer.MAX_VALUE;
         for(int i=0;i<number.length;i++)
         {
             
@@ -10,9 +11,14 @@ public class largestnumber{
                 largest = number[i];
                 
             }
+            if (smallest > number[i]) {
+                smallest = number[i];
+                
+            }
         }
-        return largest;
         
+        System.out.println("Smallest Number is : "+smallest);
+        return largest;
     }
 
 public static void main(String[] args) {
