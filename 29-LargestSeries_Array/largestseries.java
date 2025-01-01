9434418eda8ab690905014aa5a65dde9a0124991
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-01 18:16:41 +0000

    Commit Successful

--- 29-LargestSeries_Array/largestseries.java
+++ 29-LargestSeries_Array/largestseries.java
@@ -1,3 +1,11 @@
+//Initialization with Integer.MIN_VALUE and Integer.MAX_VALUE:
+
+/*largest is initialized to Integer.MIN_VALUE, which is the smallest possible integer value (-2,147,483,648). 
+This ensures that no matter what numbers are in the array, any number in the array will be larger than Integer.MIN_VALUE, 
+so largest will get updated with the first element in the array.
+
+smallest is initialized to Integer.MAX_VALUE, which is the largest possible integer value (2,147,483,647). 
+This ensures that the first number in the array will be smaller than Integer.MAX_VALUE, and smallest will be updated with the first element in the array.*/
 
 
 public class largestseries {
