44c8ac5e2b86015e506d9ad21addc7b259ef3f60
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-02 17:08:33 +0000

    Commit Successful

--- 30-Reverse_Array/revarray.java
+++ 30-Reverse_Array/revarray.java
@@ -1,5 +1,36 @@
-package 30-Reverse_Array;
+
 
 public class revarray {
+
+    public static void reverse(int number[])
+    {
+        int first =0 , last=number.length-1;
+          
+        while (first < last)
+        {
+           int  temp=0;
+           temp = last;
+           last = first;
+           first = temp;
+
+           first++;
+           last--;
+        
+        }
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2,4,6,8};
+        reverse(number);
+
+        for(int i=0;i<number.length;i++)
+        {
+            System.out.println(i +"");
+        }
+        System.out.println();
+
+
+
+    }
     
 }
