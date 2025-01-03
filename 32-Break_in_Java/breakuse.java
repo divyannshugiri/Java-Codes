a8a4a634eac3480c2596753dcc4774c1ff970537
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-03 19:13:31 +0000

    Commit Successful

---
+++ 32-Break_in_Java/breakuse.java
@@ -0,0 +1,24 @@
+//Break :-
+
+public class breakuse {
+
+    public static int breaks(int i){
+
+        for(i=1;i<=50;i++)
+        {
+            if(i == 22){
+              
+                break;
+            
+            }
+            System.out.println(i);
+           
+        }
+  return i;
+}
+    public static void main(String[] args) {
+        int i=1;
+        breaks(i);
+    }
+    
+}
