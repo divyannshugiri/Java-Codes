f5ba99e0fd69b62e3b88180db71c5824cfb2b25c
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-03 20:26:49 +0000

    Commit Successful

---
+++ 33-ContinueStatement_Java/demo.java
@@ -0,0 +1,20 @@
+ 
+ public class demo{
+
+    public static void example()
+    {
+        for(int i=1;i<=50;i++)
+        {
+            if(i==19){
+                continue;
+            }
+            System.out.println(i);
+        }
+        
+    }
+        public static void main(String[] args)
+        {
+            
+           example();
+        }
+    }
