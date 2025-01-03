6ba7dc8adc118a819446eb243fcfeb6c5c029261
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-03 17:58:28 +0000

    Commit Successful

--- 31-Square_Pattern/squarep.java
+++ 31-Square_Pattern/squarep.java
@@ -1,5 +1,21 @@
-package 31-Square_Pattern;
+
 
 public class squarep {
+
+    public static int pattern(int line){
+       for (line=1;line<=4;line++)
+       {
+        System.out.println("****");
+        
+       }
+       return line;
+    }
+
+
+    public static void main(String[] args) {
+
+        pattern(line);
+    
+    }
     
 }
