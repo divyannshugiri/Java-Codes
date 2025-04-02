206006fc218a92e68e83fe7124ddc5804e8f3479
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-02 18:05:31 +0000

    Commit Successful

---
+++ 35-String_Array/stringArray.java
@@ -0,0 +1,14 @@
+
+class stringArray {
+    public static void main(String[] args) {
+       String[] arr = new String[3];
+       arr[0]="hello";
+       arr[1]="hii";
+       arr[2]="whatapp";
+       
+       for(int i=0;i<arr.length;i++)
+       {
+           System.out.println(arr[i]);
+       }
+    }
+}
