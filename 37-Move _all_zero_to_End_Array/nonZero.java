d22db4591adec4337500b00e919cb0744d92c1c7
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-03 18:54:35 +0000

    Commit Successful

--- 37-Move _all_zero_to_End_Array/nonZero.java
+++ 37-Move _all_zero_to_End_Array/nonZero.java
@@ -1,35 +1,30 @@
+class NonZero {
+    static void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int nonZeroIndex = 0;
 
-
-class nonZero
-{
-    void pushZerosToEnd(int[] arr)
-    {
-        int n =arr.length;
-
-        int nonZeroIndex=0;
-
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i] != 0);
-            {                                      //Swap values
-                int temp=arr[i];          // 1 
-                arr[i]= arr[nonZeroIndex];//0
-                arr[nonZeroIndex] =temp;  //1
-                nonZeroIndex++;        // next 0 position                 [1  ,  2  , 0 , 4  ,3  ,0  ,5  0]
-            }                                                     //      [0  ,  0  , 0 , 0  ,0,  0,  0, 0]                           ]
-        }                                                         // ANS  [1  ,  2  , 4 , 3  ,5,  0,  0, 0]
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                // Swap values
+                int temp = arr[nonZeroIndex];
+                arr[nonZeroIndex] = arr[i];
+                arr[i] = temp;
+                nonZeroIndex++; // Move to the next position for non-zero elements
+                
+            }
+        }
+   
     }
 
-public static void main(String[] args) {
-    int[] arr ={1  ,  2  , 0 , 4  ,3  ,0  ,5  ,0 };
-}
 
-}       
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+        for(int num : arr)
+        {
+            System.out.println(num + "");
+        }
+       
+        }
+    }
 
