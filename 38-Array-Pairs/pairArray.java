17834cf0f7df8c1819a8fe8a2ab1de899ec7f183
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-06 11:01:06 +0530

    Create pairArray.java

---
+++ 38-Array-Pairs/pairArray.java
@@ -0,0 +1,27 @@
+public class pairArray {
+    
+    public static void pair(int[]arr) 
+    {
+        int n=arr.length;
+
+        for(int i=0;i<n;i++)
+        {
+            int current=arr[i];
+         
+            for(int j=i+1;j<n;j++)
+            {
+                System.out.println(current+","+arr[j]);
+                
+            }
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr={1,2,3,4,5,6};
+
+        pair(arr);
+
+    }
+}
