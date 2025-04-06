99bacdbc950f7eac812154b9624474c955d9df8f
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-06 11:05:28 +0530

    Create sameelementArray.java

---
+++ 39-ArraySameElement/sameelementArray.java
@@ -0,0 +1,28 @@
+public class SameelementArray {
+
+  static  public boolean findSameno(int[] nums)
+    {
+       int n=nums.length;
+      //  int i=0;
+      //  int j=i+1;
+      for(int i=0;i<n;i++)
+      {
+        for(int j=i+1;j<n;j++)
+        {
+        while (nums[i] == nums[j]) {
+            return true;
+        }
+        
+        }
+      }
+      return false;
+    }
+
+    
+
+    public static void main(String[] args) {
+        int[] nums={1,2,3,1};
+        System.out.println(findSameno(nums));
+    }
+    
+}
