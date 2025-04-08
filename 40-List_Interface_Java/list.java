293bee632d30be3a3fbc74f4f68fa40c0c7934a4
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-08 06:15:36 +0000

    Commit Successful

---
+++ 40-List_Interface_Java/list.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class list {
+
+    public static void main(String[] args)
+     {
+    List<Integer> numbers =new ArrayList<>();
+
+       numbers.add(21);
+       numbers.add(22);
+       numbers.add(23);
+
+       System.out.println(numbers);
+
+    }
+}
+
+
+
+
+
+
+
+      
