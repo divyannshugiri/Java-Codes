b9862e72ba4158f756a8fe0141665a5907021c0f
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-08 06:20:53 +0000

    Commit Successful

--- 34-RemoveElement_Array/removeelement.java
+++ 34-RemoveElement_Array/removeelement.java
@@ -1,31 +1,3 @@
 
-
-public class removeelement {
-
-    public int removeElement(int[] nums, int val) 
-    
-    {
-
-
-
-
-
-        return nums;
-}
-
-public static void main(String[] args)
-{
-
-}
-
-
-
-
-
-
-
-
-
-}
     
 
