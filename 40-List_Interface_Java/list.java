32ecb298c988490b18d4db5f9a3069ca0153f094
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-04-08 06:48:15 +0000

    Commit Successful

--- 40-List_Interface_Java/list.java
+++ 40-List_Interface_Java/list.java
@@ -7,12 +7,36 @@ public class list {
     public static void main(String[] args)
      {
     List<Integer> numbers =new ArrayList<>();
+    List<Integer> numbers1 =new ArrayList<>();
 
        numbers.add(21);
        numbers.add(22);
        numbers.add(23);
 
        System.out.println(numbers);
+
+      // methods of the Collection interface that's also available in the List interface are: GET,SET,REMOVE,ADD;
+       int element =numbers.get(0);
+       System.out.println(element);
+
+       
+
+       int removeElement =numbers.remove(0);
+       System.out.println(removeElement);
+
+          
+
+       int changeElement=numbers.set(1,32);
+       System.out.println(changeElement);
+
+        numbers.add(33);
+
+        System.out.println(numbers);
+
+        numbers1.addAll(numbers);
+        System.out.println(numbers1);
+
+
 
     }
 }
