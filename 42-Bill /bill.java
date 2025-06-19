e56b6fe41e5056eba5149182078772b1cf3e5257
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-06-19 17:42:06 +0000

    Commit Successful

---
+++ 42-Bill /bill.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class bill {
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the no of pizzas bought:");
+        int pizzas=sc.nextInt();
+
+        System.out.println("Enter the no of puffs bought:");
+        int puffs =sc.nextInt();
+
+        System.out.println("Enter the no of cold drinks bought:");
+        int coldDrinks =sc.nextInt();
+
+        System.out.println("Bill Details");
+        System.out.println("No of pizzas:"+ pizzas);
+        System.out.println("No of puffs:"+ puffs);
+        System.out.println("No of colddrinks:"+ coldDrinks);
+        
+
+        pizzas = pizzas * 100;
+        puffs = puffs * 20;
+        coldDrinks = coldDrinks * 10;
+
+        int totalPrice = pizzas + puffs +coldDrinks;
+        System.out.println("Total price ="+ totalPrice);
+        System.out.println("ENJOY THE SHOW!!!");
+
+        
+
+
+    }
+    
+}
